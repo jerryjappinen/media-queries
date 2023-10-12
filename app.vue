91d30a46ca8428a5f8f6c646247d2ccddb30eeff
CodeSandbox <support@codesandbox.io>
2023-10-12 17:02:06 +0000

    Sandbox snapshot commit

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
+<script>
+// https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_media_queries/Using_media_queries
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="app">
+    <h3><code>any-hover</code></h3>
+
+    <p>Testing whether input methods can hover</p>
+
+    <div class="test-any-hover">
+      <code class="test-any-hover-hover">any-hover: hover</code>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.app {
+  padding: 3% 6% 6% 6%;
+}
+
+/* https://developer.mozilla.org/en-US/docs/Web/CSS/@media/any-hover */
+@media (any-hover: hover) {
+  .test-any-hover-hover {
+    text-decoration: underline;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,14 @@
 // https://nuxt.com/docs/guide/directory-structure/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  // https://nuxt.com/docs/getting-started/styling
+  css: ["~/styles/global.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/shared.scss" as *;',
+        },
+      },
+    },
+  },
+});
