5aecdb029711477ff4153501ae3a65a7a77bcc92
CodeSandbox <support@codesandbox.io>
2023-10-15 11:55:44 +0000

    Sandbox snapshot commit

--- app.vue
+++ app.vue
@@ -1,20 +1,36 @@
 <script>
 // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_media_queries/Using_media_queries
+
+const queries = {
+  "any-hover": ["hover"],
+  "any-pointer": ["none", "coarse", "fine"],
+};
 </script>
 
 <template>
   <div class="app">
-    <h3><code>any-hover</code></h3>
+    <div
+      v-for="(values, property) in queries"
+      :key="property"
+      :class="`test-${property}`"
+    >
+      <h3>
+        <code>{{ property }}</code>
+      </h3>
+      <!-- <p>Testing whether input methods can hover</p> -->
 
-    <p>Testing whether input methods can hover</p>
-
-    <div class="test-any-hover">
-      <code class="test-any-hover-hover">any-hover: hover</code>
+      <div
+        v-for="value in values"
+        :key="property + '-' + value"
+        :class="`test-${property}-${value}`"
+      >
+        <code>@media ({{ property }}: {{ value }}) { /* ... */ }</code>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss">
 .app {
   padding: 3% 6% 6% 6%;
 }
@@ -25,4 +41,9 @@
     text-decoration: underline;
   }
 }
+
+/* https://developer.mozilla.org/en-US/docs/Web/CSS/@media/any-pointer */
+@include active-effect-selector(any-pointer, none);
+@include active-effect-selector(any-pointer, coarse);
+@include active-effect-selector(any-pointer, fine);
 </style>
